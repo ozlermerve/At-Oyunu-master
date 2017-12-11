d42153fd6e550253651a7d311e6470eeb4558473
harun uysal <harun_fatih@hotmail.com>
2017-12-11 11:45:19 +0300

    Initial commit

---
+++ YSOdev3AtOyunu/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace YSOdev3AtOyunu
+{
+    partial class frmAtGezdir
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbBoyutSec = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblSkor = new System.Windows.Forms.Label();
+            this.btnOlustur = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbBoyutSec
+            // 
+            this.lbBoyutSec.FormattingEnabled = true;
+            this.lbBoyutSec.Items.AddRange(new object[] {
+            "3 x 3",
+            "4 x 4",
+            "5 x 5",
+            "6 x 6",
+            "7 x 7",
+            "8 x 8",
+            "9 x 9"});
+            this.lbBoyutSec.Location = new System.Drawing.Point(320, 48);
+            this.lbBoyutSec.Name = "lbBoyutSec";
+            this.lbBoyutSec.Size = new System.Drawing.Size(70, 108);
+            this.lbBoyutSec.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.LightCoral;
+            this.label1.Location = new System.Drawing.Point(317, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Boyut seçin";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.LightCoral;
+            this.label2.Location = new System.Drawing.Point(326, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Skor :";
+            // 
+            // lblSkor
+            // 
+            this.lblSkor.AutoSize = true;
+            this.lblSkor.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSkor.ForeColor = System.Drawing.Color.LightCoral;
+            this.lblSkor.Location = new System.Drawing.Point(367, 197);
+            this.lblSkor.Name = "lblSkor";
+            this.lblSkor.Size = new System.Drawing.Size(0, 13);
+            this.lblSkor.TabIndex = 3;
+            // 
+            // btnOlustur
+            // 
+            this.btnOlustur.BackColor = System.Drawing.Color.Honeydew;
+            this.btnOlustur.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnOlustur.ForeColor = System.Drawing.Color.LightCoral;
+            this.btnOlustur.Location = new System.Drawing.Point(320, 162);
+            this.btnOlustur.Name = "btnOlustur";
+            this.btnOlustur.Size = new System.Drawing.Size(70, 23);
+            this.btnOlustur.TabIndex = 4;
+            this.btnOlustur.Text = "Oluştur";
+            this.btnOlustur.UseVisualStyleBackColor = false;
+            this.btnOlustur.Click += new System.EventHandler(this.btnOlustur_Click);
+            // 
+            // frmAtGezdir
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(416, 338);
+            this.Controls.Add(this.btnOlustur);
+            this.Controls.Add(this.lblSkor);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbBoyutSec);
+            this.Name = "frmAtGezdir";
+            this.Text = "Atı Gezdir";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbBoyutSec;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblSkor;
+        private System.Windows.Forms.Button btnOlustur;
+    }
+}
+
---
+++ YSOdev3AtOyunu/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YSOdev3AtOyunu
+{
+    public partial class frmAtGezdir : Form
+    {
+        public frmAtGezdir()
+        {
+            InitializeComponent();
+        }
+
+        Button[,] labirent = new Button[0,0];
+        int deger = 0, son = 0, mod = 0, sayac = 1, adIntX, adIntY;
+        string[,] koordinat = new string[8, 2];
+        string adX = "", adY = "";
+
+        private void btnOlustur_Click(object sender, EventArgs e)
+        {
+            if (lbBoyutSec.SelectedItem == "3 x 3")
+                mod = 3;
+            else if (lbBoyutSec.SelectedItem == "4 x 4")
+                mod = 4;
+            else if (lbBoyutSec.SelectedItem == "5 x 5")
+                mod = 5;
+            else if (lbBoyutSec.SelectedItem == "6 x 6")
+                mod = 6;
+            else if (lbBoyutSec.SelectedItem == "7 x 7")
+                mod = 7;
+            else if (lbBoyutSec.SelectedItem == "8 x 8")
+                mod = 8;
+            else if (lbBoyutSec.SelectedItem == "9 x 9")
+                mod = 9;
+            else
+            {
+                mod = 0;
+                MessageBox.Show("Seçim yapın.");
+            }
+            
+            labirent = new Button[mod, mod];
+            int i, j;
+            for (i = 0; i < mod; i++)
+            {
+                for (j = 0; j < mod; j++)
+                {
+                    labirent[i, j] = new Button();
+                    labirent[i, j].Width = 30;
+                    labirent[i, j].Height = 30;
+                    labirent[i, j].Left = 30 + (i * 30);
+                    labirent[i, j].Top = 30 + (j * 30);
+                    labirent[i, j].Name = (i).ToString() + "-" + (j).ToString();
+                    labirent[i, j].ForeColor = Color.RosyBrown;
+                    if ((i + j) % 2 == 0) labirent[i, j].BackColor = Color.Honeydew;
+                    else labirent[i, j].BackColor = Color.Bisque;
+
+                    labirent[i, j].Click += new EventHandler(Tikla);
+                    this.Controls.Add(labirent[i, j]);
+                }
+            }
+        }
+
+        private void Tikla(object sender, EventArgs e)
+        {
+            ((Button)sender).Text = sayac.ToString();
+            lblSkor.Text = sayac.ToString();
+            sayac++;
+
+            for (int i = 0; i < mod; i++)
+            {
+                for (int j = 0; j < mod; j++)
+                {
+                    if ((i + j) % 2 == 0) labirent[i, j].BackColor = Color.Honeydew;
+                    else labirent[i, j].BackColor = Color.Bisque;
+                }
+            }
+
+            adX = (((Button)sender).Name).Split('-')[1];
+            adY = (((Button)sender).Name).Split('-')[0];
+            adIntX = Convert.ToInt32(adX);
+            adIntY = Convert.ToInt32(adY);
+
+            KoordinatBul(adIntX, adIntY);
+            KonumGosterKontrol();
+            KonumGoster();
+
+            OyunSonMu();
+        }
+
+        public void KoordinatBul(int adIntX, int adIntY)
+        {
+            koordinat[0, 0] = (adIntX - 2).ToString();
+            koordinat[0, 1] = (adIntY - 1).ToString();
+            koordinat[1, 0] = (adIntX - 2).ToString();
+            koordinat[1, 1] = (adIntY + 1).ToString();
+            koordinat[2, 0] = (adIntX - 1).ToString();
+            koordinat[2, 1] = (adIntY + 2).ToString();
+            koordinat[3, 0] = (adIntX + 1).ToString();
+            koordinat[3, 1] = (adIntY + 2).ToString();
+            koordinat[4, 0] = (adIntX + 2).ToString();
+            koordinat[4, 1] = (adIntY + 1).ToString();
+            koordinat[5, 0] = (adIntX + 2).ToString();
+            koordinat[5, 1] = (adIntY - 1).ToString();
+            koordinat[6, 0] = (adIntX + 1).ToString();
+            koordinat[6, 1] = (adIntY - 2).ToString();
+            koordinat[7, 0] = (adIntX - 1).ToString();
+            koordinat[7, 1] = (adIntY - 2).ToString();
+        }
+
+        public void KonumGosterKontrol()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                int X = Convert.ToInt32(koordinat[i, 0]);
+                int Y = Convert.ToInt32(koordinat[i, 1]);
+                if (X < 0 || X >= mod)
+                    koordinat[i, 0] = "boyama";
+                if (Y < 0 || Y >= mod)
+                    koordinat[i, 0] = "boyama";
+            }
+
+            for (int i = 0; i < mod; i++)
+            {
+                for (int j = 0; j < mod; j++)
+                {
+                    labirent[i, j].Enabled = false;
+                }
+            }
+        }
+
+        public void KonumGoster()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                if (koordinat[i, 0] != "boyama")
+                {
+                    if (labirent[Convert.ToInt32(koordinat[i, 1]), Convert.ToInt32(koordinat[i, 0])].Text == "")
+                    {
+                        labirent[Convert.ToInt32(koordinat[i, 1]), Convert.ToInt32(koordinat[i, 0])].BackColor = Color.LightCoral;
+                        labirent[Convert.ToInt32(koordinat[i, 1]), Convert.ToInt32(koordinat[i, 0])].Enabled = true;
+                    }
+                }
+            }
+        }
+
+        public void OyunSonMu()
+        {
+            int son = 0;
+            for (int i = 0; i < mod; i++)
+            {
+                for (int j = 0; j < mod; j++)
+                {
+                    if (labirent[i, j].BackColor == Color.LightCoral)
+                        son = 1;
+                }
+            }
+            if (son == 0)
+                MessageBox.Show("Oyun bitti!" + Environment.NewLine + "Skorunuz : " + lblSkor.Text);
+        }
+    }
+}
